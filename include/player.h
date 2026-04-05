e86118d42f76b2e1ae212c43ab12ea635a667fe4
ALAE-EDDINE BOUTAIB <a.boutaib0947@uca.ac.ma>
2026-04-05 19:22:12 +0100

    Added complete player and obstacle logic

---
+++ include/player.h
@@ -0,0 +1,23 @@
+#include <raylib.h>
+#include <iostream>
+
+struct player
+{
+    //player properties
+    float position_x;
+    float position_y;
+    float width;
+    float height;
+    int lives;
+
+    //movements
+    float horizontal_movement;
+    float yspeed;
+    float gravity;
+    
+    //methods
+    player(float start_x, float start_y);
+    void update(float groundy);
+    void Draw();
+
+};
--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,45 @@
-#include <SFML/Graphics.hpp>
+#include "../include/player.h"
+#include "../include/obstacle.h"
 
-int main()
-{
-    sf::RenderWindow window(sf::VideoMode({200, 200}), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+using namespace std;
 
-    while (window.isOpen())
-    {
-        while (const std::optional event = window.pollEvent())
-        {
-            if (event->is<sf::Event::Closed>())
-                window.close();
+int main () {
+
+    cout << "starting the game"<<endl;
+    const int screen_width = 1280;
+    const int screen_height = 800;
+    bool gameover = false;
+    bool is_moving = true;
+    int player_start_x = 150;
+    int player_start_y = 150;
+    float groundy = screen_height - 100 ;
+    type_of_obstacle typeofobstacle = type_of_obstacle::tall_obstacle;
+
+    InitWindow(screen_width, screen_height, "platformer: game of the century");
+
+    SetTargetFPS(60);
+
+    obstacle the_first_obstacle(400, groundy - 100 , typeofobstacle, is_moving);
+    player my_player(player_start_x, player_start_y);
+
+    //main game loop
+    while(WindowShouldClose() == false) {
+
+        if(!gameover) {
+            my_player.update(groundy);
+            the_first_obstacle.update();
         }
 
-        window.clear();
-        window.draw(shape);
-        window.display();
+
+        BeginDrawing();
+        ClearBackground(BLACK);
+        my_player.Draw();
+        the_first_obstacle.Draw();
+
+        EndDrawing();
     }
+
+
+    CloseWindow();
+    return 0;
 }
